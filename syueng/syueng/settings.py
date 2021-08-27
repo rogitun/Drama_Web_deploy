abee6e1b4edeed5516d89b0b18cad4b1dd51a9ed
hansel <rogitun@gmail.com>
2021-08-27 22:41:43 +0900

    almost done

--- syueng/syueng/settings.py
+++ syueng/syueng/settings.py
@@ -134,8 +134,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rogitun@gmail.com'
-EMAIL_HOST_PASSWORD = 'ushbhpbgxiklupzn'
+EMAIL_HOST_USER = "rogitun@gmail.com"
+EMAIL_HOST_PASSWORD = get_secret('GG_PASSWORD')
 
 
 # Static files (CSS, JavaScript, Images)
