67a85335be3892c7352984cdbe859f9d763915f9
rogitun <81704910+rogitun@users.noreply.github.com>
2021-12-04 22:23:13 +0900

    Update settings.py

--- syueng/syueng/settings.py
+++ syueng/syueng/settings.py
@@ -144,10 +144,10 @@ USE_L10N = True
 USE_TZ = False
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "rogitun@gmail.com"
+EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = get_secret('GG_PASSWORD')
 
 
